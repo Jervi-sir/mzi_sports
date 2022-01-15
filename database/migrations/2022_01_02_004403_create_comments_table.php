ddbd7b92861cefea1b558af97bd195554e6bec97
jervi <Jervi>
2022-01-15 01:11:35 +0100

    added column in comment table

--- database/migrations/2022_01_02_004403_create_comments_table.php
+++ database/migrations/2022_01_02_004403_create_comments_table.php
@@ -17,7 +17,7 @@ class CreateCommentsTable extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->foreignId('post_id')->constrained()->onDelete('cascade');
-            $table->foreignId('parent_id')->constrained()->nullable();
+            $table->integer('parent_id')->unsigned()->nullable();
             $table->mediumText('comment');
             $table->timestamps();
         });
